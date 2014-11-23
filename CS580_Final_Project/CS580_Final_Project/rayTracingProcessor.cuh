5c4b9c7263eb888703ee543c7500a35e63bc152c
z530989673 <530989673@qq.com>
2014-11-22 16:27:49 -0800

    memory optimization

--- CS580_Final_Project/CS580_Final_Project/rayTracingProcessor.cuh
+++ CS580_Final_Project/CS580_Final_Project/rayTracingProcessor.cuh
@@ -11,6 +11,7 @@ static  __device__ float3 *vertexBuffer_CUDA,*normalBuffer_CUDA;
 static  __device__ uchar4 *colorBuffer_CUDA;
 static  __device__ Material *materialBuffer_CUDA;
 static  __device__ uchar1 *materialIndexBuffer_CUDA;
+static __device__ Camera* mainCamera_CUDA;
 
 static __device__ Photon *photonBuffer_CUDA;
 void rayTracingCuda(uchar4 * pixels,int count,float3* vertex,float3* normal,uchar4* color, Photon* photons, Material* materials, uchar1* materialIndex);
